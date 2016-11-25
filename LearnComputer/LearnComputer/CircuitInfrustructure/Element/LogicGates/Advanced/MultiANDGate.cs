e18af149393d3d375e0d71932118427e61d0fd80
jxw00100 <343275209@qq.com>
2016-11-25 15:29:41 +0800

    Add Multi input AND / OR gate

---
+++ LearnComputer/LearnComputer/CircuitInfrustructure/Element/LogicGates/Advanced/MultiANDGate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LearnComputer.CircuitInfrustructure
+{
+    public class MultiANDGate
+    {
+        private Relay[] _relays;
+        public IInputEndpoint[] Inputs { get; private set; }
+        public IOutputEndpoint Output { get; private set; }
+
+        public MultiANDGate(Int32 inputCount)
+        {
+            if(inputCount < 2) throw new ArgumentException("Input ports must be 2 or more.");
+
+            _relays = new Relay[inputCount];
+            Inputs = new IInputEndpoint[inputCount];
+
+            for(var i= 0; i<inputCount; i++)
+            {
+                _relays[i] = new Relay();
+                Inputs[i] = _relays[i].Input;
+                if (i > 0)
+                {
+                    _relays[i].InputOfContact.ConnectTo(_relays[i-1].Output);
+                }
+            }
+            Output = _relays[inputCount - 1].Output;
+        }
+
+        public void ConnectInputsWith(params IEndpoint[] connectPoints)
+        {
+            var pointsCount = connectPoints.Length;
+            if (pointsCount > Inputs.Length) throw new ArgumentException("The connected points are too many.");
+            for (var i = 0; i < pointsCount; i++)
+            {
+                Inputs[i].ConnectTo(connectPoints[i]);
+            }
+        }
+    }
+}
---
+++ LearnComputer/LearnComputer/CircuitInfrustructure/Element/LogicGates/Advanced/MultiORGate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LearnComputer.CircuitInfrustructure
+{
+    public class MultiORGate
+    {
+        private Relay[] _relays;
+        private Nexus _nexus;
+        public IInputEndpoint[] Inputs { get; private set; }
+        public IOutputEndpoint Output { get; private set; }
+
+        public MultiORGate(Int32 inputCount)
+        {
+            if(inputCount < 2) throw  new ArgumentException("Input ports must be 2 or more.");
+
+            _relays = new Relay[inputCount];
+            _nexus = new Nexus(inputCount + 1);
+            Inputs = new IInputEndpoint[inputCount];
+
+            for(var i= 0; i<inputCount; i++)
+            {
+                _relays[i] = new Relay();
+                Inputs[i] = _relays[i].Input;
+                _nexus.ConnectAt(_relays[i].Output, i);
+            }
+            Output = _nexus.GetEndpointAt(inputCount);
+        }
+
+        public void ConnectInputsWith(params IEndpoint[] connectPoints)
+        {
+            var pointsCount = connectPoints.Length;
+            if (pointsCount > Inputs.Length) throw new ArgumentException("The connected points are too many.");
+            for (var i = 0; i < pointsCount; i++)
+            {
+                Inputs[i].ConnectTo(connectPoints[i]);
+            }
+        }
+    }
+}
