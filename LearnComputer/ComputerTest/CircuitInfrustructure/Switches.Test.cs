c3acd2ccf00c07885d04e8821c719fdb6ac566a8
jxw00100 <343275209@qq.com>
2016-11-26 14:17:49 +0800

    8 bit switches

---
+++ LearnComputer/ComputerTest/CircuitInfrustructure/Switches.Test.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using LearnComputer.CircuitInfrustructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerTest.CircuitInfrustructure
+{
+    [TestClass]
+    public class SwitchesTest
+    {
+        Switches8Bits _switches = new Switches8Bits();
+        IndicatorLight _light1 = new IndicatorLight();
+        IndicatorLight _light2 = new IndicatorLight();
+        IndicatorLight _light3 = new IndicatorLight();
+        IndicatorLight _light4 = new IndicatorLight();
+        IndicatorLight _light5 = new IndicatorLight();
+        IndicatorLight _light6 = new IndicatorLight();
+        IndicatorLight _light7 = new IndicatorLight();
+        IndicatorLight _light8 = new IndicatorLight();
+
+        public SwitchesTest()
+        {
+            _switches.Connect(_light1.Input,
+                              _light2.Input,
+                              _light3.Input,
+                              _light4.Input,
+                              _light5.Input,
+                              _light6.Input,
+                              _light7.Input,
+                              _light8.Input);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _switches.Clear();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+        }
+
+        [TestMethod]
+        public void DefaultInitialize()
+        {
+            Switches8Bits switches = new Switches8Bits();
+            Assert.IsNotNull(switches);
+        }
+
+        [TestMethod]
+        public void InitializeWithSettings()
+        {
+            Switches8Bits switches = new Switches8Bits(false, true, false, true, true, false, true, false);
+            Assert.IsNotNull(switches);
+            Assert.AreEqual(switches.Outputs[0].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[1].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[2].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[3].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[4].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[5].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[6].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[7].LastSentSignal, 0);
+        }
+
+        [TestMethod]
+        public void InitializeWithNotFullSettings()
+        {
+            Switches8Bits switches = new Switches8Bits(false, true, false, true);
+            Assert.IsNotNull(switches);
+            Assert.AreEqual(switches.Outputs[0].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[1].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[2].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[3].LastSentSignal, 1);
+            Assert.AreEqual(switches.Outputs[4].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[5].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[6].LastSentSignal, 0);
+            Assert.AreEqual(switches.Outputs[7].LastSentSignal, 0);
+        }
+
+        [TestMethod]
+        public void SetFull()
+        {
+            _switches.Set(false, true, false, true, true, false, true, false);
+
+            Assert.IsFalse(_light1.Lighting);
+            Assert.IsTrue(_light2.Lighting);
+            Assert.IsFalse(_light3.Lighting);
+            Assert.IsTrue(_light4.Lighting);
+            Assert.IsTrue(_light5.Lighting);
+            Assert.IsFalse(_light6.Lighting);
+            Assert.IsTrue(_light7.Lighting);
+            Assert.IsFalse(_light8.Lighting);
+        }
+
+        [TestMethod]
+        public void SetNotFull()
+        {
+            _switches.Set(false, true, false, true);
+
+            Assert.IsFalse(_light1.Lighting);
+            Assert.IsTrue(_light2.Lighting);
+            Assert.IsFalse(_light3.Lighting);
+            Assert.IsTrue(_light4.Lighting);
+            Assert.IsFalse(_light5.Lighting);
+            Assert.IsFalse(_light6.Lighting);
+            Assert.IsFalse(_light7.Lighting);
+            Assert.IsFalse(_light8.Lighting);
+        }
+
+        [TestMethod]
+        public void Clear()
+        {
+            _switches.Set(true, true, true, true, true, true, true, true);
+
+            Assert.IsTrue(_light1.Lighting);
+            Assert.IsTrue(_light2.Lighting);
+            Assert.IsTrue(_light3.Lighting);
+            Assert.IsTrue(_light4.Lighting);
+            Assert.IsTrue(_light5.Lighting);
+            Assert.IsTrue(_light6.Lighting);
+            Assert.IsTrue(_light7.Lighting);
+            Assert.IsTrue(_light8.Lighting);
+
+            _switches.Clear();
+
+            Assert.IsFalse(_light1.Lighting);
+            Assert.IsFalse(_light2.Lighting);
+            Assert.IsFalse(_light3.Lighting);
+            Assert.IsFalse(_light4.Lighting);
+            Assert.IsFalse(_light5.Lighting);
+            Assert.IsFalse(_light6.Lighting);
+            Assert.IsFalse(_light7.Lighting);
+            Assert.IsFalse(_light8.Lighting);
+        }
+    }
+}
---
+++ LearnComputer/LearnComputer/CircuitInfrustructure/Element/Switches8Bits.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace LearnComputer.CircuitInfrustructure
+{
+    public class Switches8Bits
+    {
+        private const Int32 WIDTH = 8;
+        private Boolean[] _switches;
+        public IOutputEndpoint[] Outputs { get; private set; }
+        private Boolean[] _defaults;
+
+        public Switches8Bits(params Boolean[] switchesOn)
+        {
+            _switches = new Boolean[WIDTH];
+            _defaults = new Boolean[WIDTH];
+            InitializeOutputs();
+            
+            Set(switchesOn);
+        }
+
+        private void InitializeOutputs()
+        {
+            Outputs = new IOutputEndpoint[WIDTH];
+
+            for (var i = 0; i < WIDTH; i++)
+            {
+                Outputs[i] = new OutputEndpoint();
+            }
+        }
+
+        public void Set(params Boolean[] switchesOn)
+        {
+            if (switchesOn != null)
+            {
+                for (var i = 0; i < WIDTH && i < switchesOn.Length; i++)
+                {
+                    _switches[i] = switchesOn[i];
+                }
+            }
+            DispatchSignal();
+        }
+
+        private void DispatchSignal()
+        {
+            for (var i = 0; i < WIDTH; i++)
+            {
+                Outputs[i].Produce(Convert.ToInt32(_switches[i]));
+            }
+        }
+
+        public void Connect(params IEndpoint[] points)
+        {
+            for (var i = 0; i < WIDTH && i < points.Length; i++)
+            {
+                Outputs[i].ConnectTo(points[i]);
+            }
+        }
+
+        public void Clear()
+        {
+            Set(_defaults);
+        }
+    }
+}
