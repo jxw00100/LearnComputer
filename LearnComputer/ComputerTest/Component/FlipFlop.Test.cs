72a2d7224ac95af019a9479a139a2120220922f5
jxw00100 <343275209@qq.com>
2017-01-13 16:18:50 +0800

    Simplest Flip-Flop

---
+++ LearnComputer/ComputerTest/Component/FlipFlop.Test.cs
@@ -0,0 +1,89 @@
+﻿using LearnComputer.CircuitInfrustructure;
+using LearnComputer.Component;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerTest.Component
+{
+    [TestClass]
+    public class FlipFlopTest
+    {
+        private PowerSupplier _powerSet, _powerClear;
+        private IndicatorLight _light;
+        private FlipFlop _flipFlop;
+
+        public FlipFlopTest()
+        {
+            _powerSet = new PowerSupplier();
+            _powerClear = new PowerSupplier();
+            _light = new IndicatorLight();
+            _flipFlop = new FlipFlop();
+
+            _flipFlop.Set.ConnectTo(_powerSet.Output);
+            _flipFlop.Clear.ConnectTo(_powerClear.Output);
+            _flipFlop.Output.ConnectTo(_light.Input);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _powerSet.Off();
+            _powerClear.Off();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+        }
+
+        [TestMethod]
+        public void CreateNew()
+        {
+            Assert.IsNotNull(_flipFlop);
+        }
+
+        [TestMethod]
+        public void AbleToSet()
+        {
+            _powerSet.On();
+            Assert.IsTrue(_light.Lighting);
+        }
+
+        [TestMethod]
+        public void AbleToClear()
+        {
+            _powerSet.On();
+            _powerClear.On();
+            Assert.IsFalse(_light.Lighting);
+        }
+
+        [TestMethod]
+        public void SetIsLatched()
+        {
+            _powerSet.On();
+            _powerSet.Off();
+            Assert.IsTrue(_light.Lighting);
+
+            _powerSet.On();
+            _powerSet.Off();
+            _powerSet.On();
+            _powerSet.Off();
+            Assert.IsTrue(_light.Lighting);
+        }
+
+        [TestMethod]
+        public void ClearIsLatched()
+        {
+            _powerSet.On();
+            _powerSet.Off();
+            _powerClear.On();
+            _powerClear.Off();
+            Assert.IsFalse(_light.Lighting);
+
+            _powerClear.Off();
+            _powerClear.On();
+            _powerClear.Off();
+            _powerClear.Off();
+            Assert.IsFalse(_light.Lighting);
+        }
+    }
+}
---
+++ LearnComputer/LearnComputer/Component/FlipFlop/FlipFlop.cs
@@ -0,0 +1,27 @@
+﻿using LearnComputer.CircuitInfrustructure;
+
+namespace LearnComputer.Component
+{
+    public class FlipFlop
+    {
+        private NORGate _norGate1, _norGate2;
+        private TShapedNexus _nexus;
+
+        public IInputEndpoint Set { get; private set; }
+        public IInputEndpoint Clear { get; private set; }
+        public IOutputEndpoint Output { get; private set; }
+
+        public FlipFlop()
+        {
+            _norGate1 = new NORGate();
+            _norGate2 = new NORGate();
+            _nexus = new TShapedNexus(_norGate2.Output, _norGate1.Input1);
+
+            _norGate1.Output.ConnectTo(_norGate2.Input1);
+
+            Set = _norGate1.Input2;
+            Clear = _norGate2.Input2;
+            Output = _nexus.GetEndpointAt(2);
+        }
+    }
+}
