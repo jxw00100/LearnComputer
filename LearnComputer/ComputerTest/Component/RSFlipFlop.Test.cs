23b4ad19c8a54e70535939f8fd986a90ba276a1c
jxw00100 <343275209@qq.com>
2017-01-13 16:35:28 +0800

    RSFlipFlop

---
+++ LearnComputer/ComputerTest/Component/RSFlipFlop.Test.cs
@@ -0,0 +1,100 @@
+﻿using LearnComputer.CircuitInfrustructure;
+using LearnComputer.Component;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerTest.Component
+{
+    [TestClass]
+    public class RSFlipFlopTest
+    {
+        private PowerSupplier _powerSet, _powerReset;
+        private IndicatorLight _lightSet, _lightReset;
+        private RSFlipFlop _RS;
+
+        public RSFlipFlopTest()
+        {
+            _powerSet = new PowerSupplier();
+            _powerReset = new PowerSupplier();
+            _lightSet = new IndicatorLight();
+            _lightReset = new IndicatorLight();
+            _RS = new RSFlipFlop();
+
+            _RS.Set.ConnectTo(_powerSet.Output);
+            _RS.Reset.ConnectTo(_powerReset.Output);
+            _RS.Q.ConnectTo(_lightSet.Input);
+            _RS.QBar.ConnectTo(_lightReset.Input);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _powerSet.Off();
+            _powerReset.Off();
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+        }
+
+        [TestMethod]
+        public void CreateNew()
+        {
+            Assert.IsNotNull(_RS);
+        }
+
+        [TestMethod]
+        public void AbleToSet()
+        {
+            _powerSet.On();
+            Assert.IsTrue(_lightSet.Lighting);
+            Assert.IsFalse(_lightReset.Lighting);
+        }
+
+        [TestMethod]
+        public void AbleToReset()
+        {
+            _powerReset.On();
+            Assert.IsFalse(_lightSet.Lighting);
+            Assert.IsTrue(_lightReset.Lighting);
+        }
+
+        [TestMethod]
+        public void SetIsLatched()
+        {
+            _powerSet.On();
+            Assert.IsTrue(_lightSet.Lighting);
+            Assert.IsFalse(_lightReset.Lighting);
+
+            _powerSet.Off();
+            Assert.IsTrue(_lightSet.Lighting);
+            Assert.IsFalse(_lightReset.Lighting);
+
+            _powerSet.On();
+            _powerSet.Off();
+            _powerSet.On();
+            _powerSet.Off();
+            Assert.IsTrue(_lightSet.Lighting);
+            Assert.IsFalse(_lightReset.Lighting);
+        }
+
+        [TestMethod]
+        public void ResetIsLatched()
+        {
+            _powerReset.On();
+            Assert.IsFalse(_lightSet.Lighting);
+            Assert.IsTrue(_lightReset.Lighting);
+
+            _powerReset.Off();
+            Assert.IsFalse(_lightSet.Lighting);
+            Assert.IsTrue(_lightReset.Lighting);
+
+            _powerReset.On();
+            _powerReset.Off();
+            _powerReset.On();
+            _powerReset.Off();
+            Assert.IsFalse(_lightSet.Lighting);
+            Assert.IsTrue(_lightReset.Lighting);
+        }
+    }
+}
---
+++ LearnComputer/LearnComputer/Component/FlipFlop/RSFlipFlop.cs
@@ -0,0 +1,28 @@
+﻿using LearnComputer.CircuitInfrustructure;
+
+namespace LearnComputer.Component
+{
+    public class RSFlipFlop
+    {
+        private NORGate _norGate1, _norGate2;
+        private TShapedNexus _nexus1, _nexus2;
+
+        public IInputEndpoint Set { get; private set; }
+        public IInputEndpoint Reset { get; private set; }
+        public IOutputEndpoint Q { get; private set; }
+        public IOutputEndpoint QBar { get; private set; }
+
+        public RSFlipFlop()
+        {
+            _norGate1 = new NORGate();
+            _norGate2 = new NORGate();
+            _nexus1 = new TShapedNexus(_norGate1.Output, _norGate2.Input1);
+            _nexus2 = new TShapedNexus(_norGate2.Output, _norGate1.Input1);
+
+            Reset = _norGate1.Input2;
+            Set = _norGate2.Input2;
+            Q = _nexus1.GetEndpointAt(2);
+            QBar = _nexus2.GetEndpointAt(2);
+        }
+    }
+}
